d1ec3d223471b58d4a89db4e7f757f02b86974bb
Arafat Islam Khairul <108567092+arafatkhairul@users.noreply.github.com>
2023-10-06 21:36:04 +0600

    Init express socket

---
+++ App.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require("cors");
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+        origin: "http://localhost:5173"
+    }
+});
+
+
+// Serve your React.js application from here, or you can create a separate Express route for it.
+
+app.use(cors());
+
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Handle socket events here
+    socket.on('message', (data) => {
+        console.log('Message received:', data);
+        // Broadcast the message to all connected clients
+        io.emit('message', data);
+    });
+    // Join room 
+    socket.on('joinRoom', (RoomId) => {
+        socket.join(RoomId);
+        socket.to(RoomId).emit("newJoinMessage", "New user join this meeting");
+    })
+
+
+    // offer 
+    socket.on("sendTheOffer", (offer, RoomId) => {
+        socket.to(RoomId).emit("reciveOffer", offer);
+    })
+
+    // send the answer 
+    socket.on("sendTheAnswer", (answer, RoomId) => {
+        socket.to(RoomId).emit("reciveAnswer", answer);
+    })
+
+    // send the icecandidate
+    socket.on("sendIcecandidate", (icecandidate, RoomId) => {
+        socket.to(RoomId).emit("reciveIcecandidate", icecandidate);
+    })
+
+
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+app.get('/', (req, res) => {
+    res.send("Hello socket server")
+})
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
